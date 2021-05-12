39dca728ea14f785e628ce752586b1e3466b8ba7
GYBDaniel <g.gyenesdani@gmail.com>
2021-05-12 08:59:08 +0200

    frissitve

---
+++ X0YBW4_0428/feldolgozo2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="hu">
+ <head>
+  <meta charset="UTF-8">
+  <meta name="Generator" content="EditPlus®">
+  <meta name="Author" content="">
+  <meta name="Keywords" content="">
+  <meta name="Description" content="">
+  <title>Urlap vezerlese uj elemekkel</title>
+ </head>
+ <body>
+  <font style="font-size:18pt">
+  <h2>Urlap vezerlese uj elemekkel</h2>
+  <?php
+  print ("Név: " . $_GET['nev'] . "<br>");
+print ("PIN kód: " . $_GET['kod'] . "<br>");
+  ?>
+  <a href="urlap4.html"> vissza az ürlapra...</a>
+ </font>
+ </body>
+</html>
